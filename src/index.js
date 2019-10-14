7991289e1f64f765bcbc51a519ce01f6ebdad1d8
Bartek Błęcki <bartek@blecki.pl>
2019-10-14 15:12:28 +0200

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Router, hashHistory} from 'react-router';
-import {Provider} from 'react-redux';
-import store from './store/index';
+import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store';
 import routes from './routes';
-import DevTools from './DevTools';
 
 render(
     <Provider store={store}>
-        <div>
-            <Router history={hashHistory} routes={routes}/>
-            <DevTools/>
-        </div>
+        <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
 );
